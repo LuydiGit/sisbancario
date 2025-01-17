061935edc2bb019cde485e9ae8828639cab1d1cf
LuydiGit <luydisousa@gmail.com>
2025-01-16 21:07:33 -0300

    conection db_1

--- banco-1/index.js
+++ banco-1/index.js
@@ -2,10 +2,30 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import mysql from 'mysql2';
+
 import dotenv from 'dotenv';
 
 // Carregar variáveis de ambiente
 dotenv.config();
+
+// Criar conexão com o banco de dados MySQL
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,  // Exemplo: localhost ou IP do seu servidor
+  user: process.env.DB_USER,  // Seu usuário do MySQL
+  password: process.env.DB_PASSWORD,  // Sua senha do MySQL
+  database: process.env.DB_NAME  // Nome do seu banco de dados
+});
+
+// Verificar a conexão com o banco de dados
+db.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar com o banco de dados:', err);
+    return;
+  }
+  console.log('Conectado ao banco de dados MySQL');
+});
+
 
 const app = express();
 const PORT = process.env.PORT || 5001;
@@ -18,8 +38,6 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.status(200).send('API funcionando!');
 });
-
-
 
 // Iniciar o servidor
 app.listen(PORT, () => {
